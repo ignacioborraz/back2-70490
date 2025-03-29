f05e57b38ca5898eb503029a7e3da42e2b3d613a
ignacioborraz <ignacioborraz@hotmail.com>
2025-03-29 09:48:17 -0300

    fix

--- src/helpers/dbConnect.helper.js
+++ src/helpers/dbConnect.helper.js
@@ -1,8 +1,14 @@
 import { connect } from "mongoose";
 
-const dbConnect = async () => {
+/**
+ * @dbConnect
+ * se conecta a la base de datos
+ * recibe un link de conexión a mongo
+ * registra en la consola el éxito o el fracaso
+ */
+const dbConnect = async (url) => {
   try {
-    connect(process.env.MONGO_URL);
+    connect(url);
     console.log("mongo database connected");
   } catch (error) {
     console.log(error);
--- src/helpers/hash.helper.js
+++ src/helpers/hash.helper.js
@@ -6,21 +6,19 @@ import { genSaltSync, hashSync, compareSync } from "bcrypt";
  * devuelve una contraseña hasheada
  */
 function createHash(password) {
-    const salt = genSaltSync(10)
-    const hashPassword = hashSync(password, salt)
-    return hashPassword
+  const salt = genSaltSync(10);
+  const hashPassword = hashSync(password, salt);
+  return hashPassword;
 }
 
 /**
  * @verifyHash
- * recibir una contraseña cruda (la del formulario) y la contraseña de la base de datos
- * las va a comparar
- * y devuelve el booleano correspondiente
+ * recibe una contraseña cruda (la del formulario) y la contraseña de la base de datos
+ * las compara y devuelve el booleano correspondiente
  */
 function verifyHash(password, mongoPassword) {
-    //console.log({password, mongoPassword});
-    const verify = compareSync(password, mongoPassword)
-    return verify
+  const verify = compareSync(password, mongoPassword);
+  return verify;
 }
 
-export { createHash, verifyHash }
+export { createHash, verifyHash };
